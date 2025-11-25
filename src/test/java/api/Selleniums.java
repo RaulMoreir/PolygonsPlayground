6b0310fe6e01e5211d2b37170ece3969659e3d2d
RaulA <raulanencar@gmail.com>
2025-11-25 14:02:52 -0300

    web scraping updates

--- src/test/java/api/Selleniums.java
+++ src/test/java/api/Selleniums.java
@@ -1,11 +1,14 @@
 package api;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class Selleniums {
     @Test
@@ -21,10 +24,11 @@ public class Selleniums {
         botao.click();
         String newURL = driver.getCurrentUrl();
         Assert.assertEquals(newURL, "https://www.claudiokussleiloes.com.br/relacao-foto/845");
+        driver.quit();
     }
 
     @Test
-    private void a(){
+    private void a() {
         String url = "https://www.claudiokussleiloes.com.br";
         WebDriver driver = new ChromeDriver();
         driver.get(url);
@@ -34,9 +38,25 @@ public class Selleniums {
         WebElement botao = driver.findElement(By.xpath("//*[@id=\"slider_home\"]/div/div/div/div/div[1]/div/a"));
         botao.click();
 
+        WebElement getUrlList = driver.findElement(By.xpath("//a[contains(text(),'Ver em formato em lista')]"));
+        String listUrl = getUrlList.getAttribute("href");
+        driver.get(listUrl);
 
-    };
+        List<WebElement> linhas = driver.findElements(By.cssSelector("table tr"));
+        int quantidadeVeiculos = linhas.size();
+
+        for(int i  = 1;  i < quantidadeVeiculos; i++){
+            String runningCar = "https://www.claudiokussleiloes.com.br/lance/845/"+i;
+            driver.get(runningCar);
+
+            WebElement GetCarName = driver.findElement(By.tagName("h1"));
+            String car = GetCarName.getText();
+            System.out.println(car);
+        }
+
+        driver.quit();
 
 
+    }
 }
 
